1ae7b4701e270946d0c6f2dd07ff348622200ef1
Jonathan Merritt <j.s.merritt@gmail.com>
2016-01-16 20:24:51 +1100

    Change comment indent

--- src/Data/Spiner.hs
+++ src/Data/Spiner.hs
@@ -9,8 +9,8 @@ type Weights = Matrix Double
 
 -- | Perform feed-forward propagation of a multi-layered ANN.
 --
--- NOTE: The matrix of weights for each layer must be 1 column wider than the number of 
---       inputs, with the first column being a weight for the constant factor.
+--   NOTE: The matrix of weights for each layer must be 1 column wider than the number of 
+--         inputs, with the first column being a weight for the constant factor.
 feedForward :: ActivationFn   -- ^ activation function for all neurons
             -> [ Weights ]    -- ^ matrices of layer weights
             -> Vector Double  -- ^ input vector (excluding constant term)
@@ -19,15 +19,15 @@ feedForward g ws i = foldl' (evalLayer g) i ws
 
 -- | Evaluate the outputs of a single layer in a feed-forward ANN.
 --
--- This evaluates (apologies for the pseudo-math layout):
---   y = g ( W * [ 1 ] )
---         (     [ x ] )
--- Where y is the output vector, x is the input vector, W are the weights and g is the
--- activation function. g is applied to every element of the resulting vector
--- independently.
+--   This evaluates (apologies for the pseudo-math layout):
+--     y = g ( W * [ 1 ] )
+--           (     [ x ] )
+--   Where y is the output vector, x is the input vector, W are the weights and g is the
+--   activation function. g is applied to every element of the resulting vector
+--   independently.
 --
--- NOTE: The matrix of weights must be 1 column wider than the input vector, with the
---       first column being a weight for the constant factor.
+--   NOTE: The matrix of weights must be 1 column wider than the input vector, with the
+--         first column being a weight for the constant factor.
 evalLayer :: ActivationFn    -- ^ activation function for all neurons
           -> Vector Double   -- ^ input vector (excluding constant term), length n
           -> Weights         -- ^ matrix of layer weights, size m x (n + 1)
