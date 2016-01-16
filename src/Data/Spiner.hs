a7616d43bce7fb5c8c2b9cfa8e83ee06d30b0991
Jonathan Merritt <j.s.merritt@gmail.com>
2016-01-16 17:20:07 +1100

    Feed-forward evaluation

--- src/Data/Spiner.hs
+++ src/Data/Spiner.hs
@@ -1,3 +1,14 @@
 module Data.Spiner where
 
+import Data.Foldable (foldl')
+import qualified Data.Vector.Storable as VS
+import Numeric.LinearAlgebra
 
+type ActivationFn = Double -> Double
+type Weights = Matrix Double
+
+feedForward :: ActivationFn -> [ Weights ] -> Vector Double -> Vector Double
+feedForward g ws i = foldl' (evalLayer g) i ws
+
+evalLayer :: ActivationFn -> Vector Double -> Weights -> Vector Double
+evalLayer g i w = VS.map g (w #> i)
